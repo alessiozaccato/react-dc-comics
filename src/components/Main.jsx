caaaadba63544f25c2460bf116e4add3c999eb4c
alessiozaccato <alessiozaccato@gmail.com>
2025-02-18 17:33:33 +0100

    ADD: main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,56 @@
+const Main = () => {
+    return (
+        <main>
+            <section className="contentSection container">
+                <p>Content Goes Here</p>
+            </section>
+            <div className="bgBlue">
+                <section className="infoSection container">
+
+                    <div>
+                        <figure>
+                            <img src="src/assets/img/buy-comics-digital-comics.png" alt="digital comics" />
+
+                        </figure>
+                        <p>digital comics</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="src/assets/img/buy-comics-merchandise.png" alt="merchandise" />
+
+                        </figure>
+                        <p>dc merchandise</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="src/assets/img/buy-comics-subscriptions.png" alt="subscriptions" />
+
+                        </figure>
+                        <p>subscription</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="src/assets/img/buy-comics-shop-locator.png" alt="shop locator" />
+
+                        </figure>
+                        <p>comic shop location</p>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="src/assets/img/buy-dc-power-visa.svg" alt="power visa" />
+                        </figure>
+                        <p>dc power visa</p>
+                    </div>
+
+
+
+
+
+                </section>
+            </div>
+
+        </main>
+    )
+}
+
+export default Main
