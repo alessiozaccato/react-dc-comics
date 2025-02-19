7546c028b9e611f59fccd1d56e8a51e0fe60031d
alessiozaccato <alessiozaccato@gmail.com>
2025-02-19 15:47:19 +0100

    ADD: headerLinks from appjs with props in headerjsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+
+  const headerLinks = [
+    { id: 1, title: 'characters', url: '#' },
+    { id: 2, title: 'comics', url: '#' },
+    { id: 3, title: 'movies', url: '#' },
+    { id: 4, title: 'tv', url: '#' },
+    { id: 5, title: 'games', url: '#' },
+    { id: 6, title: 'collectibles', url: '#' },
+    { id: 7, title: 'videos', url: '#' },
+    { id: 8, title: 'fans', url: '#' },
+    { id: 9, title: 'news', url: '#' },
+    { id: 10, title: 'shop', url: '#' }
+  ]
+
   return (
     <>
-      <Header />
+      <Header headerLinks={headerLinks} />
       <Main />
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 import logo from "../assets/img/dc-logo.png"
 
-const Header = () => {
+const Header = (props) => {
 
     // let's do array of objects for the links
-    const headerLinks = [
-        { id: 1, title: 'characters', url: '#' },
-        { id: 2, title: 'comics', url: '#' },
-        { id: 3, title: 'movies', url: '#' },
-        { id: 4, title: 'tv', url: '#' },
-        { id: 5, title: 'games', url: '#' },
-        { id: 6, title: 'collectibles', url: '#' },
-        { id: 7, title: 'videos', url: '#' },
-        { id: 8, title: 'fans', url: '#' },
-        { id: 9, title: 'news', url: '#' },
-        { id: 10, title: 'shop', url: '#' }
-    ]
+    // const headerLinks = [
+    //     { id: 1, title: 'characters', url: '#' },
+    //     { id: 2, title: 'comics', url: '#' },
+    //     { id: 3, title: 'movies', url: '#' },
+    //     { id: 4, title: 'tv', url: '#' },
+    //     { id: 5, title: 'games', url: '#' },
+    //     { id: 6, title: 'collectibles', url: '#' },
+    //     { id: 7, title: 'videos', url: '#' },
+    //     { id: 8, title: 'fans', url: '#' },
+    //     { id: 9, title: 'news', url: '#' },
+    //     { id: 10, title: 'shop', url: '#' }
+    // ]
+
+    const { headerLinks } = props;
 
     return (
         <header className="container">
